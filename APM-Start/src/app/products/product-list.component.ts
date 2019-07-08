c21cb9ccb082d1914c26fdce4f50918f1b459660
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2019-07-08 23:32:20 +0100

    get/set

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -9,7 +9,7 @@ import { ProductService } from './product.service';
 })
 export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
-    listFilter: string;
+    // listFilter: string;
     showImage: boolean;
 
     imageWidth: number = 50;
@@ -18,6 +18,16 @@ export class ProductListComponent implements OnInit {
 
     filteredProducts: IProduct[];
     products: IProduct[];
+
+    private _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.performFilter(this.listFilter);
+    }
 
     constructor(private productService: ProductService) { }
 
@@ -43,4 +53,11 @@ export class ProductListComponent implements OnInit {
             this.filteredProducts = this.products;
         }
     }
+
+    // used for the long two way binding
+
+    // onFilterChange(filter: string) : void {
+    //     this.listFilter = filter;
+    //     this.performFilter(this.listFilter);
+    // }
 }
