66237b50d0e2af15070267519cb6a29cfa209316
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2019-07-13 12:19:51 +0100

    OnChanges

--- APM-Start/src/app/shared/criteria/criteria.component.ts
+++ APM-Start/src/app/shared/criteria/criteria.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, AfterViewInit, ElementRef, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ElementRef, Output, EventEmitter, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { NgModel } from '@angular/forms';
 import { IProduct } from '../../products/product';
 
@@ -7,25 +7,37 @@ import { IProduct } from '../../products/product';
   templateUrl: './criteria.component.html',
   styleUrls: ['./criteria.component.css']
 })
-export class CriteriaComponent implements OnInit, AfterViewInit {
+export class CriteriaComponent implements OnInit, AfterViewInit, OnChanges {
   
   listFilter: string;
-
+  hitMessage: string;
+  
   @ViewChild('filterElement') filterElementRef: ElementRef; 
   @Input() displayDetail: boolean;
   @Input() hitCount: number;
-
+  
   constructor() { }
-
+  
   ngAfterViewInit(): void {
     if (this.filterElementRef.nativeElement != null) {
-
+      
       this.filterElementRef.nativeElement.focus();
     }
   }
-
+  
   ngOnInit() {
+    
+  }
 
+  // alternative way for watching for changes on the @Input() property
+  ngOnChanges(changes: SimpleChanges): void { 
+    console.log(changes);
+
+    if (changes['hitCount'] && !changes['hitCount'].currentValue) {
+      this.hitMessage = 'No matches found';
+    } else {
+      this.hitMessage = 'Hits: ' + this.hitCount;
+    }
   }
   
 
